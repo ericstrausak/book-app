c5a40f83c36303d018e72cb0bd47da6b7fc49740
ericstrausak <straueri@students.zhaw.ch>
2025-01-06 16:52:01 +0100

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,18 @@
 <script>
     import ComponentA from "$lib/components/ComponentA.svelte";
-    
-
-
-
 </script>
-<div class= "container svelte-9e0nj2">
+
+<div class="container svelte-9e0nj2">
     <h1>Welcome to MyBookReviews</h1>
-    <p>Your guide to achieve the reading experience that every Book worm wishes to achieve.</p>
-    <a href="/books" class="btn btn-primary svelte-9e0nj2">All Books on one click</a>
+    <p>
+        Your guide to achieve the reading experience that every Book worm wishes
+        to achieve.
+    </p>
+    <a href="/books" class="btn btn-primary svelte-9e0nj2"
+        >All Books on one click</a
+    >
     <a href="/reviews" class="btn btn-primary svelte-9e0nj2">Reviews</a>
-    </div>
-    <div clas="random-books svelte-9e0nj2"><h2 class="svelte-9e0nj2">Books you might like</h2>
-        <p class="loading svelte-9e0nj2">Searching for Books...</p>
-            
-        </div>
-
-
+</div>
 
 <style>
-   
 </style>
-
-
-    
