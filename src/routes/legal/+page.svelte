70b9d926cf7f1843b7594d50daa83dc340e471ae
ericstrausak <straueri@students.zhaw.ch>
2025-01-06 22:32:57 +0100

    changes

---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container svelte-9e0nj2">
+    <h1>Legal Information</h1>
+    <p>
+        Welcome to <strong>MyBookReviews</strong>. This platform is designed to create, share, and discover book reviews.
+    Please note that all content is user-generated, and we do not take responsibility for the accuracy of the reviews.
+    If you have any questions or concerns, please feel free to contact us.
+    </p>
+</div>
