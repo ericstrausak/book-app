570a75f2cebdeae3a8fd5d0d02048b8a6172bd95
ericstrausak <straueri@students.zhaw.ch>
2025-01-06 16:14:41 +0100

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 </script>
 <div class= "container svelte-9e0nj2">
-    <h1 class="svelte-9e0nj2">Welcome to MyBookReviews</h1>
+    <h1>Welcome to MyBookReviews</h1>
     <p>Your guide to achieve the reading experience that every Book worm wishes to achieve.</p>
     <a href="/books" class="btn btn-primary svelte-9e0nj2">All Books on one click</a>
     <a href="/reviews" class="btn btn-primary svelte-9e0nj2">Reviews</a>
